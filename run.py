865a6d9c9e4a41344e9c8d2f979f481fc01738df
Esteban Carvallo <crewxart@gmail.com>
2022-06-17 15:58:21 -0400

    executable_path, headless mode

--- run.py
+++ run.py
@@ -1,13 +1,18 @@
 import argparse
 
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+
 from selenium.webdriver.firefox.service import Service
 
 parser = argparse.ArgumentParser(description="Open url with firefox webdriver")
 parser.add_argument("--url", type=str, required=True, help="url to open in browser")
 
 args = parser.parse_args()
+options = FirefoxOptions()
+options.headless = True
 
-service = Service(log_path="/dev/null")
-driver = webdriver.Firefox(service=service)
+service = Service(executable_path="/home/pin/.local/bin/geckodriver")
+driver = webdriver.Firefox(options=options, service=service)
 driver.get(args.url)
+print("Finish")
