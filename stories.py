2c093deb8894f318f8598c9368877e161a5dbbee
Esteban Carvallo <crewxart@gmail.com>
2022-06-22 10:58:42 -0400

    Update stories.py

--- stories.py
+++ stories.py
@@ -4,19 +4,15 @@ from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
 import pandas as pd
 import hashlib
-# Config Selenium Options.
 options = FirefoxOptions()
 options.headless = True
 service = Service(executable_path="/home/pin/.local/bin/geckodriver", log_path="/dev/null") ### DOCKER PATH
-#service = Service(executable_path="./deps/geckodriver.exe") ### WINDOWS PATH
 driver = webdriver.Firefox(options=options, service=service)
 # Def to encrypt the img src.
 def filenameEncrypted(str):
     hash_object = hashlib.md5(str.encode())
     md5_hash = hash_object.hexdigest()
     return md5_hash+'.png'
-# RUN :) .
-# Go to instructions URL.
 driver.get('https://as.com/diarioas/america.html')
 # In this case, we need to **visit the iframe url to scrape** into the page content
 # Because, the DOM content loaded never showed the selectors of the elements that
@@ -25,7 +21,6 @@ driver.get('https://as.com/diarioas/america.html')
 # Go iFrame URL.
 src = driver.find_element(by=By.CSS_SELECTOR, value='.mat-mix-anchor').get_attribute('src')
 driver.get(src)
-# Access to all stories content to extract href, description and ss w/ filename encrypted.
 stories_data = driver.find_elements(by=By.CSS_SELECTOR, value='.slick-track a')
 stories = []
 for story in stories_data:
@@ -40,9 +35,7 @@ for story in stories_data:
     stories.append(story_dict)
     # Take SS to div content.
     story.screenshot('/home/pin/app/assets/ads/'+story_dict['filename']) ### DOCKER PATH.
-    #story.screenshot('assets/ads/'+story_dict['filename']) ## WINDOWS PATH.
 # Export stories[] to CSV (stories.csv)
 df = pd.DataFrame(stories)
 df.to_csv('/home/pin/app/assets/stories.csv', index=False) ### DOCKER PATH
-#df.to_csv('assets/stories.csv', index=False) ### WINDOWS PATH
 driver.close()
